9b963f966b0ac32650b3c6d6737aa50019d976bb
coyoteazul <hernanruiz91@gmail.com>
2021-03-15 22:38:36 -0300

    Ejercicio1

--- CAI_Ejercicio1/Program.cs
+++ CAI_Ejercicio1/Program.cs
@@ -6,7 +6,17 @@ namespace CAI_Ejercicio1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            for (int i = 1; i <= 805; i++)
+            {
+                string sufix = "";
+                if (i % 3 == 0)
+                    sufix += "Foo";
+                if (i % 5 == 0)
+                    sufix += "Bar";
+
+                Console.WriteLine(i + sufix);
+            }
+
         }
     }
 }
